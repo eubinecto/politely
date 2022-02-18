94811724efedc01123ac0255c74d8d391b03f177
eubinecto <eubinecto>
2022-02-18 07:22:25 +0000

    [#7] tuner와 visualiser 분리

---
+++ explore/explore_pd_index_slice.py
@@ -0,0 +1,15 @@
+from fetchers import fetch_rules
+import pandas as pd
+
+
+def main():
+    rules = fetch_rules()
+    styler = pd.DataFrame(rules).transpose().style
+    subset = pd.IndexSlice[("friend", "private")]
+    print(subset)
+    print(rules[subset[0]][subset[1]])  # 아하.
+
+
+
+if __name__ == '__main__':
+    main()
--- politetune/tuner.py
+++ politetune/tuner.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import numpy as np
 import pandas as pd
 from typing import Optional, List, Tuple
@@ -19,23 +21,27 @@ class Tuner:
     def __init__(self):
         self.kiwi = Kiwi()
         self.polite: Optional[bool] = None
+        self.listener: Optional[str] = None
+        self.visibility: Optional[str] = None
+        self.tokens: Optional[List[Token]] = None
 
-    def __call__(self, sent: str, listener: str, visibility: str) -> dict:
-        self.polite = self.RULES[listener][visibility]
+    def __call__(self, sent: str, listener: str, visibility: str) -> str:
         # preprocess the sentence
         tuned = sent + "." if not sent.endswith(".") else sent  # for accurate pos-tagging
         tuned = tuned.replace(" ", " " * 2)  # for accurate spacing
         # tokenize the sentence, and replace all the EFs with their honorifics
-        tokens = self.kiwi.tokenize(tuned)
+        self.tokens = self.kiwi.tokenize(tuned)
+        # choose the subsets... here!
+        rules_subset = pd.IndexSlice[(listener, visibility)]
         texts = [
             self.HONORIFICS[f"{token.form}+{token.tag}"][self.polite]
             if f"{token.form}+{token.tag}" in self.HONORIFICS.keys()
             else token.form
-            for token in tokens
+            for token in self.tokens
         ]
         # restore spacings
-        starts = np.array([token.start for token in tokens] + [0])
-        lens = np.array([token.len for token in tokens] + [0])
+        starts = np.array([token.start for token in self.tokens] + [0])
+        lens = np.array([token.len for token in self.tokens] + [0])
         sums = np.array(starts) + np.array(lens)
         spacings = (starts[1:] - sums[:-1]) > 0
         tuned = "".join([
@@ -48,22 +54,6 @@ class Tuner:
         # post-process the sentence
         if not sent.endswith("."):
             tuned = tuned.replace(".", "")
-        return {
-            "tuned": tuned,
-            "rules": self.highlight_rules(listener, visibility),
-            "honorifics": self.highlight_honorifics(tokens)
-        }
+        return tuned
 
-    def highlight_rules(self, listener: str, visibility: str) -> Styler:
-        styler = pd.DataFrame(self.RULES).transpose().style
-        subset = pd.IndexSlice[(listener, visibility)]
-        styler = styler.applymap(lambda x: "background-color: purple", subset=subset)
-        return styler
 
-    def highlight_honorifics(self, tokens: Token) -> Styler:
-        styler = pd.DataFrame(self.HONORIFICS).transpose().style
-        for token in tokens:
-            if f"{token.form}+{token.tag}" in self.HONORIFICS.keys():
-                subset = pd.IndexSlice[(f"{token.form}+{token.tag}", self.polite)]
-                styler = styler.applymap(lambda x: "background-color: purple", subset=subset)
-        return styler
