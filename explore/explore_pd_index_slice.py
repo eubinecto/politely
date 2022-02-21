817460f9caf716a5e72a669e53e08dd99bb46e75
eubinecto <eubinecto>
2022-02-21 06:41:32 +0000

    [#7] Separating Highlighters from Tuner. Aggregating them into processors.py

--- explore/explore_pd_index_slice.py
+++ explore/explore_pd_index_slice.py
@@ -6,6 +6,8 @@ def main():
     rules = fetch_rules()
     styler = pd.DataFrame(rules).transpose().style
     subset = pd.IndexSlice[("friend", "private")]
+    styler = styler.applymap(lambda x: x, subset=("friend", "private"))
+    print(type(subset))
     print(subset)
     print(rules[subset[0]][subset[1]])  # 아하.
 
