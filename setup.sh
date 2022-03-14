1bf5ab76cf62ddec3aed1ff72fe4deda3b27cf01
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-14 16:59:55 +0000

    [#22] ubuntu-20-compatible command

--- setup.sh
+++ setup.sh
@@ -13,12 +13,11 @@ port = $PORT\n\
 " > ~/.streamlit/config.toml
 
 # for building khaiii
-pip install cmake   # installing cmake is essential
 git clone https://github.com/kakao/khaiii.git
 cd khaiii || exit
 mkdir build
 cd build || exit
-cmake .. || exit
+cmake -E env CXXFLAGS="-w" cmake .. || exit
 make all resource || exit
 make install || exit
 make package_python || exit
