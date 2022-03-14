9ee1d24e8803b7649a398ea54814bd2cd2eaa4a2
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-14 16:38:58 +0000

    [#22] setup.sh fixed

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
-# git clone khaiii
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "tlrndk123@gmail.com"n
+
+echo "\
+[general]\n\
+email = \"tlrndk123@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
