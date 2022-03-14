71caacd2bc47d78f7c400c4d587771c38649acf6
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-14 16:52:07 +0000

    [#22] installing khaiii on setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,15 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+# for building khaiii
+git clone https://github.com/kakao/khaiii.git
+cd khaiii || exit
+mkdir build
+cd build || exit
+cmake .. || exit
+make all resource || exit
+make install || exit
+make package_python || exit
+cd package_python || exit
+pip install . || exit
