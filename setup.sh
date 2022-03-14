7adbb5adcae840f13be455b3f24997074bec338f
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-14 16:19:45 +0000

    [#22] setup.sh file updated

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 # git clone khaiii
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "tlrndk123@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
+" > ~/.streamlit/config.toml
