68d50c88cb34249084061a5625cf2b2a90eed799
Eu-Bin KIM <tlrndk123@gmail.com>
2022-03-14 16:55:21 +0000

    [#22] installing cmake prior to building khaiii

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ port = $PORT\n\
 " > ~/.streamlit/config.toml
 
 # for building khaiii
+pip install cmake   # installing cmake is essential
 git clone https://github.com/kakao/khaiii.git
 cd khaiii || exit
 mkdir build
