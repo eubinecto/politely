ba729c8f68c2303c822c9bc54ac15b6d8950e8ee
Eu-Bin KIM <tlrndk123@gmail.com>
2022-05-15 11:32:41 +0900

    [#62] failing tests 완성

---
+++ explore/explore_khaiii_dae.py
@@ -0,0 +1,11 @@
+from khaiii import KhaiiiApi
+
+
+def main():
+    api = KhaiiiApi()
+    for token in api.analyze("밥먹고 바로 누우면 안 된대."):
+        print(token)
+
+
+if __name__ == '__main__':
+    main()
