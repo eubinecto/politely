3cdc220c13d2eb9ce58490670f94af905d09f3dd
Eu-Bin KIM <tlrndk123@gmail.com>
2022-07-10 16:51:26 +0100

    experimental - playing with spacy's Korean model

---
+++ explore/explore_spacy_ko_core_news_md.py
@@ -0,0 +1,16 @@
+"""
+Korean models are now available.
+https://spacy.io/models/ko#ko_core_news_md
+It has been available since April 2022.
+"""
+import spacy
+
+nlp = spacy.load('ko_core_news_md')
+
+for token in nlp("아버지가 방에 들어가신다."):
+    print(token.text,
+          token.lemma_,
+          token.pos_,
+          token.tag_)
+
+# but well, I think kiwi is far more accruate than that.
