df91682f30c8ff7c661c808da7402a8a7a9510a4
Eu-Bin KIM <tlrndk123@gmail.com>
2023-05-14 16:26:59 +0900

    [#111]: `main_demo_contextual` -  맥락 고려 X, 맥락 고려 O 추가

--- main_demo_contextual.py
+++ main_demo_contextual.py
@@ -1,6 +1,11 @@
 from politely import Styler
 
-styler = Styler(lm_search=True)  # lm_search 기능 추가
-print("##### 맥락 고려 예시 1) - 쳥유 ######")
-print(styler("자, 이제 먹어요.", 2))
+styler = Styler(lm_search=False)  # lm_search 기능 추가
+print("##### lm을 쓰지 않는 경우 맥락 고려 X ######")
 print(styler("난 밥을 먹어요.", 2))
+print(styler("자, 이제 먹어요.", 2))
+
+styler = Styler(lm_search=True)  # lm_search 기능 추가
+print("##### lm을 쓰는 경우 맥락 고려 O ######")
+print(styler("난 밥을 먹어요.", 2))
+print(styler("자, 이제 먹어요.", 2))
