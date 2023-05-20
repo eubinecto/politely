a58265ecfbd43fd359c1c1ec5d6b2a35a0027a10
Eu-Bin KIM <tlrndk123@gmail.com>
2023-05-21 08:34:54 +0900

    [#111]: some comments

--- main_demo_contextual.py
+++ main_demo_contextual.py
@@ -2,11 +2,11 @@ from politely import Styler
 from politely.modeling_gpt2_scorer import GPT2Scorer
 from politely.modeling_heuristic_scorer import HeuristicScorer
 
-styler = Styler(scorer=HeuristicScorer())  # lm_search 기능 추가
+styler = Styler(scorer=HeuristicScorer())
 print("##### lm을 쓰지 않는 경우 맥락 고려 X ######")
 print(styler("내일 저랑 같이 점심 먹어요.", 0))
 
-styler = Styler(scorer=GPT2Scorer())  # lm_search 기능 추가
+styler = Styler(scorer=GPT2Scorer())  # uses GPT2Scorer by default
 print("##### lm을 쓰는 경우 맥락 고려 O ######")
 print(styler("내일 저랑 같이 점심 먹어요.", 0))
 
