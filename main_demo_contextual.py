264004c8f36d43c3aa43f8d420b56d4ea6a4164a
Eu-Bin KIM <tlrndk123@gmail.com>
2023-05-21 08:18:56 +0900

    [#111]: readme update

--- main_demo_contextual.py
+++ main_demo_contextual.py
@@ -10,11 +10,8 @@ print(styler("내일 같이 점심 먹어요.", 0))
 
 
 """
-styler = Styler(lm_search=False)  # lm_search 기능 추가
-print("##### lm을 쓰지 않는 경우 맥락 고려 X ######")
-print(styler("내일 같이 점심 먹어요.", 0))
-
-styler = Styler(lm_search=True)  # lm_search 기능 추가
-print("##### lm을 쓰는 경우 맥락 고려 O ######")
-print(styler("내일 같이 점심 먹어요.", 0))
+##### lm을 쓰지 않는 경우 맥락 고려 X ######
+내일 같이 점심 먹어.
+##### lm을 쓰는 경우 맥락 고려 O ######
+내일 같이 점심 먹자.
 """
