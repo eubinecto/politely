4cb6f23d1153c8151f3cd7b518b8d9a2cd9325a3
Eu-Bin KIM <tlrndk123@gmail.com>
2023-05-21 08:42:36 +0900

    [#111]: fixed dependencies

--- politely/__init__.py
+++ politely/__init__.py
@@ -1,4 +1,4 @@
 from politely.rules import SEP, TAG, NULL, SELF, RULES, PREFERENCES, CASUAL, POLITE, FORMAL  # noqa
 from politely.styler import Styler  # noqa
 
-__version__ = "v3.3.0"
+__version__ = "v3.3.1"
